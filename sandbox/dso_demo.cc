2ce2708d9e33fc873d55ce183c2cb92e4596b9b3
Thomas Köppe <tkoeppe@google.com>
2021-01-14 12:18:03 +0000

    [dso_demo] Add some non-trivial constructions (e.g. using TLS)
    The use of TLS from DSOs presents particularly interesting technical
    challenges.
    
    The use of TLS in general triggers a certain problem in Bazel on MacOS
    at this point, which can be avoided by compiling with
    --dynamic_mode=off or by disabling the dynamic mode feature
    entirely (see https://github.com/bazelbuild/bazel/issues/4341#issuecomment-758028203).

--- sandbox/dso_demo.cc
+++ sandbox/dso_demo.cc
@@ -1,5 +1,23 @@
 #include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <string_view>
 
-extern "C" void demo_function() {
-  std::puts("I am a function.");
+#include "absl/strings/string_view.h"
+#include "absl/container/flat_hash_map.h"
+
+extern "C" void demo_function(std::size_t n, const char* args[]) {
+  std::puts("I am a function. Here come my arguments.");
+
+  absl::flat_hash_map<std::string, std::vector<std::string>> m;
+
+  for (std::size_t i = 0; i != n; ++i) {
+    absl::string_view arg = args[i];
+    m[arg].emplace_back().assign(arg);
+  }
+
+  for (const auto& [k, v] : m) {
+    std::printf("Argument '%*s' appears %zu times.\n",
+                static_cast<int>(k.size()), k.data(), v.size());
+  }
 }
